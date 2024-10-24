9fc7769c75b81591c07fc5ed1759af8b5209e4ce
Rick Spencer <rick@influxdata.com>
2024-10-24 15:36:28 -0400

    adding benchmark code

---
+++ bench.py
@@ -0,0 +1,31 @@
+import time
+from gpt4all import GPT4All
+
+results = []
+devices = ["cpu","gpu"]
+models = ["Meta-Llama-3-8B-Instruct.Q4_0.gguf",
+          "Phi-3-mini-4k-instruct.Q4_0.gguf",
+          "orca-mini-3b-gguf2-q4_0.gguf"]
+
+def generate(model_name, device):
+    model = GPT4All(model_name, device=device)
+    print(f"model created for {model_name}/{device}")
+    start_time = time.time()
+    response = model.generate("Who was Jerry Garcia?")
+    end_time = time.time()
+    
+    latency = end_time - start_time
+    print(f"response generated in {latency}")
+    result = {"model":model_name, "device":device, "response":response,"latency":latency}
+    results.append(result)
+
+for model in models:
+    for device in devices:
+        generate(model, device)
+
+print(f"model       device      latency")
+for result in results:
+    print(f"{result['model'][:10]}      {result['device']}      {result['latency']}")
+
+
+
